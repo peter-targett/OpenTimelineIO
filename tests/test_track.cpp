6e45c4baa19b00282cc462c452805fb1824e433a
Peter Targett <petert@filmlight.ltd.uk>
2026-09-18 09:50:36 +0100

    Restore the argument order in Track constructor.
    Update test.
    
    Signed-off-by: Peter Targett <petert@filmlight.ltd.uk>

--- tests/test_track.cpp
+++ tests/test_track.cpp
@@ -148,6 +148,21 @@ main(int argc, char** argv)
             std::find(items.begin(), items.end(), clip.value) != items.end());
     });
 
+    tests.add_test("test_constructor_markers", [] {
+        SerializableObject::Retainer<Marker> ma0 = new Marker("marker0");
+        SerializableObject::Retainer<Marker> ma1 = new Marker("marker1");
+        SerializableObject::Retainer<Track>  tr  = new Track(
+            "track",
+            std::nullopt,
+            Track::Kind::video,
+            AnyDictionary(),
+            std::nullopt,
+            std::vector<Marker*>({ ma0, ma1 }));
+        assertEqual(tr->markers().size(), 2);
+        assertEqual(tr->markers()[0].value, ma0.value);
+        assertEqual(tr->markers()[1].value, ma1.value);
+    });
+
     tests.run(argc, argv);
     return 0;
 }
